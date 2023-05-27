cf27f2d0702871f319f7266d659b711dd9cb2747
Prince Bioh <princebioh15@gmail.com>
2023-05-27 19:23:37 +0000

    Add tab Pages

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
+import 'first_tab.dart';
+import 'second_tab.dart';
+import 'third_tab.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(
-          Icons.account_tree_rounded,
-          size: 40,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.account_tree_rounded,
+            size: 30,
+          ),
+          centerTitle: false,
+          title: const Text(
+            "Flutter",
+            style: TextStyle(fontSize: 25),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.camera_enhance_outlined),
+              splashRadius: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              splashRadius: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+              splashRadius: 30,
+            ),
+          ],
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: 'Home',
+            ),
+            Tab(
+              text: 'Status',
+            ),
+            Tab(
+              text: 'Contacts',
+            ),
+          ]),
         ),
-        centerTitle: false,
-        title: const Text(
-          "Flutter",
-          style: TextStyle(fontSize: 30),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.camera_enhance_outlined),
-            splashRadius: 30,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-            splashRadius: 30,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-            splashRadius: 30,
-          ),
-        ],
+        body: const TabBarView(children: [
+          FirstTab(),
+          SecondTab(),
+          ThirdTab(),
+        ]),
       ),
     );
   }
